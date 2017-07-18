ffda24ac9f9e6d5f195c8df85f5ece2307173e6f
Gunar C. Gessner <gunar@gunargessner.com>
2017-07-18 13:34:04 -0300

    feat: allow partial application and prevent arg override

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 'use strict'
 
 function curryNamed (keys, fn) {
-  return function helper (o) {
+  return function helper (cache) {
     return function (...objects) {
-      const obj = Object.assign({}, o, ...objects)
-      return includes(keys, Object.keys(obj)) ? fn(obj) : helper(obj)
+      const incoming = Object.assign({}, ...objects)
+      const argsOverridden = includesSome(Object.keys(cache), Object.keys(incoming))
+      const error = `The following arguments were overidden: ${argsOverridden}`
+      if (argsOverridden.length > 0) throw Error(error)
+      const obj = Object.assign({}, cache, ...objects)
+      return includesEvery(keys, Object.keys(obj)) ? fn(obj) : helper(obj)
     }
   }({})
 }
 
-const includes = (mandatory, received) =>
+const includesEvery = (mandatory, received) =>
   mandatory.every(key =>
+    received.includes(key))
+
+const includesSome = (mandatory, received) =>
+  mandatory.filter(key =>
     received.includes(key))
 
 module.exports = curryNamed
--- test.js
+++ test.js
@@ -6,31 +6,40 @@ const foo = curry(
   ['a', 'b', 'c'],
   ({a, b, c}) => a + b + c)
 
-test('original function', function (t) {
+test('original function', t => {
   const actual = foo({ a: 1, b: 2, c: 4})
   const expected = 7
   t.equal(actual, expected)
   t.end()
 })
 
-test('currying', function (t) {
+test('currying', t => {
   const actual = foo({ a: 1 })({ b: 2, c: 4})
   const expected = 7
   t.equal(actual, expected)
   t.end()
 })
 
-test('partial application (simple)', function (t) {
+test('partial application (simple)', t => {
   const actual = foo({ a: 1 })({ b: 2 }, { c: 4 })
   const expected = 7
   t.equal(actual, expected)
   t.end()
 })
 
-test('partial application (lodash)', function (t) {
+test('partial application (lodash)', t => {
   const partiallyApplied = partial(foo)([{ a: 1 }])
   const actual = partiallyApplied({ b: 2 })({ c: 4 })
   const expected = 7
   t.equal(actual, expected)
   t.end()
 })
+
+test('argument overriding', t => {
+  try {
+    foo({ a: 1 })({ a: 1, b: 2, c: 4})
+  } catch (e) {
+    return t.end()
+  }
+})
+
